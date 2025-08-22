76f76e9f480adc6befb423f7da47cc2c95381e78
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-08-21 21:05:40 -0300

    add desafios extras 21/08

---
+++ Vetorunidimensional/desafios/Desafio3/assets/js/main.js
@@ -0,0 +1,13 @@
+function ValidaMultiplicador() {
+
+    let vetor = [];
+
+    for(let i = 0 ; i<5; i++){
+
+        vetor[i] = parseInt(prompt("Favor informe o "+(i+1)+"° número: "));
+
+
+    }
+
+    
+}
